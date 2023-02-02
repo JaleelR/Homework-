b7578bd23d9b17618dd72f53d2601e1e580c365b
Jaleel Whaley <jw430118@gmail.com>
2023-02-02 12:48:01 -0500

    add global

---
+++ global.js
@@ -0,0 +1,25 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+
+
+//answer 
+let PI = 3.14
+PI = 42;
+
+
+
+/*What is the difference between var and let?
+-- Var is global and let is a block varible , you also can not redeclarelet but you reassign
+
+
+What is the difference between var and const?
+-- you cannot reassign and redeclare with const but you can  with var
+
+
+What is the difference between let and const?
+let allows you to reassign with let varible but you cant with const
+
+
+What is hoisting?
+hoisting is calling a var varible before it is actaully deifined
